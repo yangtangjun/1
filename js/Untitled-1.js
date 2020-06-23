0ca0b3eb3072a6abfbb8cf739e541d4695bd994c
yangtangjun <66725138+yangtangjun@users.noreply.github.com>
2020-06-23 16:26:55 +0800

    Add files via upload

---
+++ js/Untitled-1.js
@@ -0,0 +1,12 @@
+function dlzh()
+{
+    alert("登录成功,即将进入我的博客")
+}
+function test()
+{
+    alert("忘记密码了？密码都能忘记，吃饭怎么都不忘记")
+}
+function screen()
+{
+    alert("注册？你还想注册？这种事还是在梦里想想吧")
+}
---
+++ js/Untitled-2.js
@@ -0,0 +1,4 @@
+function screen()
+{
+    alert("刷新成功，点击确定进入我的首页")
+}
